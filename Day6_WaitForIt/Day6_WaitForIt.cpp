7f9c4c681c5b106bfad4fa1f11bd61eca93dcb69
MZhikoV <m.zhikov@gmail.com>
2024-01-01 17:26:24 +0200

    Day 06 Scond part solution

--- Day6_WaitForIt/Day6_WaitForIt.cpp
+++ Day6_WaitForIt/Day6_WaitForIt.cpp
@@ -6,6 +6,78 @@
 
 using namespace std;
 
+long long int records(long long int a, long long int b); //function for determining the total count of values that are greater than the record
+
+
+int main()
+{
+    string time;
+    string dist;
+
+    getline(cin, time);
+    getline(cin, dist);
+
+    time.erase(0, 5);
+    dist.erase(0, 9);
+
+    stringstream timeS(time);
+    stringstream distS(dist);
+
+    string ttime, ddist;
+    string sNumTime, sNumDist;
+
+    long long int prod = 0;
+
+
+    while (timeS >> ttime) {
+        sNumTime += ttime;
+    }
+
+    while (distS >> ddist) {
+        sNumDist += ddist;
+    }
+    
+    long long int numTime = stoll(sNumTime);
+    long long int numDist = stoll(sNumDist);
+
+
+    
+    prod= records(numTime, numDist);
+
+
+    cout << prod << endl;
+
+    return 0;
+
+}
+
+
+long long int records(long long int a, long long int b) {
+    long long int sum = 0;
+
+    for (long long int i = 0; i <= a; i++) {
+        long long int temp = (a-i)*i;
+        if (temp > b) {
+            sum++;
+        }
+    }
+    return sum;
+}
+
+
+//20240101 1430
+
+
+//First part
+/*
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <list>
+
+using namespace std;
+
 int records(int a, int b); //function for determining the total count of values that are greater than the record
 
 
@@ -49,5 +121,4 @@ int records(int a, int b) {
     return sum;
 }
 
-
-//20240101 1430
+*/
